18b552ce422f516d4677bb78ca0be33709d8940c
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-12-09 18:21:13 +0100

    Models and superuser

--- First_Project/First_App/admin.py
+++ First_Project/First_App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from First_App.models import AccessRocord, Topic, Webpage
+
+admin.site.register(Topic)
+admin.site.register(AccessRocord)
+admin.site.register(Webpage)
+
 
 # Register your models here.
--- First_Project/First_App/models.py
+++ First_Project/First_App/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model): #this is inheritance
+    top_name = models.CharField(max_length = 300, unique = True) #unique so u don't have duplicate names
+
+    def __str__(self):
+        return self.top_name
+
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 300, unique = True)
+    url = models.URLField(unique = True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccessRocord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete = models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
