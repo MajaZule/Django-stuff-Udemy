aea8db08d152da2814fd670a379531efeab30b65
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-12-12 15:43:55 +0100

    More validators

--- third_project/basic_app/forms.py
+++ third_project/basic_app/forms.py
@@ -1,14 +1,23 @@
 from django import forms
+from django.core import validators
+
+
+#custom validation function
+def check_for_z(value):
+    if value[0].lower != 'z':
+        raise forms.ValidationError("Name needs to start with z")
 
 
 class Form_name(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators = [check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget = forms.Textarea)
-    botcatcher = forms.CharField(required = False, widget = forms.HiddenInput)
+    botcatcher = forms.CharField(required = False, widget = forms.HiddenInput, validators = [validators.MaxValueValidator(0)])
 
-    def clean_botcatcher(self):
-        botcatcher = self.cleaned_data['botcatcher']
-        if len(botcatcher) > 0:
-            raise forms.ValidationError("It's a bot!")
-        return botcatcher
+
+#we can replace this code with validators call
+    #def clean_botcatcher(self):
+        #botcatcher = self.cleaned_data['botcatcher']
+        #if len(botcatcher) > 0: #because bot would add in a value
+            #raise forms.ValidationError("It's a bot!")
+        #return botcatcher
