e842365702c1ae2cd8f3fd49c85b7dd2d6ad24b4
MajaZule <44669001+MajaZule@users.noreply.github.com>
2018-12-12 16:04:13 +0100

    Getting clean data from forms and email validation

--- third_project/basic_app/forms.py
+++ third_project/basic_app/forms.py
@@ -3,16 +3,26 @@ from django.core import validators
 
 
 #custom validation function
-def check_for_z(value):
-    if value[0].lower != 'z':
-        raise forms.ValidationError("Name needs to start with z")
+#def check_for_z(value):
+#    if value[0].lower != 'z':
+#        raise forms.ValidationError("Name needs to start with z")
 
 
 class Form_name(forms.Form):
-    name = forms.CharField(validators = [check_for_z])
+    name = forms.CharField() # also here pass into brackets validators = [check_for_z]
     email = forms.EmailField()
+    verify_email = forms.EmailField(label = 'Enter your email again: ')
     text = forms.CharField(widget = forms.Textarea)
-    botcatcher = forms.CharField(required = False, widget = forms.HiddenInput, validators = [validators.MaxValueValidator(0)])
+#    botcatcher = forms.CharField(required = False, widget = forms.HiddenInput, validators = [validators.MaxValueValidator(0)])
+
+#grabbing all clean data at once
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Please make sure emails match")
 
 
 #we can replace this code with validators call
